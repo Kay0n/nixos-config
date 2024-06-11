2da69d63a45764cd501945a04241624c818a411b
Kay0n <kayon5555@gmail.com>
2024-06-11 18:17:55 +0930

    feat: java versions avaliable in shell

--- modules/java.nix
+++ modules/java.nix
@@ -1,17 +1,20 @@
+{ pkgs, ... }:
 
-{ pkgs, ... }: {
-
-  environment.systemPackages = with pkgs; [
-    temurin-jre-bin-8
-    temurin-jre-bin-17
-  ];
-
-  programs.java = {
-    enable = true;
-    package = pkgs.temurin-jre-bin-21;
-  };
-
-}
-
-
-
+  # enable "java8" and "java17" to be used in shell
+  let
+    java8 = pkgs.writeShellScriptBin "java8" ''
+      exec ${pkgs.temurin-jre-bin-8}/bin/java "$@"
+    '';
+    java17 = pkgs.writeShellScriptBin "java17" ''
+      exec ${pkgs.temurin-jre-bin-17}/bin/java "$@"
+    '';
+  in
+  {
+    environment.systemPackages = with pkgs; [
+      temurin-jre-bin-21
+      temurin-jre-bin-8
+      temurin-jre-bin-17
+      java8 
+      java17
+    ];
+  }
