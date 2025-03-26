fe0d4ac87a340632d01f1e8d3d6f82e9e294db94
Kay0n <kayon5555@gmail.com>
2025-03-26 22:46:04 +1030

    add webdav for nginx

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -5,14 +5,24 @@
 
   services.nginx.virtualHosts = {
     "books.refract.online" = {
-        # addSSL = true;
         enableACME = true;
         forceSSL = true;
-        # root = "/var/www/";
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8083";
+        extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+        locations = {
+          "/webdav/" = {
+            proxyPass = "http://127.0.0.1:8085/";
+          };
+          "/" = {
+            proxyPass = "http://127.0.0.1:8083";
+          };
         };
     };
+
     "refract.online" = {
         enableACME = true;
         forceSSL = true;
