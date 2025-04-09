ef7a6b1094580adf396124ee92b18e74e4f05b5b
Kay0n <kayon5555@gmail.com>
2025-04-09 18:26:35 +0930

    add laptop to sync

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -16,7 +16,7 @@
         "jdy-desktop" = { id = "JIMQ75S-RWRJ4XZ-L2ILZXN-FSEIOL2-OJY2J4P-ZJ72XOQ-6NFOJRW-EQ3TCA7"; };
         "oracle-main" = { id = "QGSXIVA-3JQP6G5-D25L2BP-AD4J5N5-T7H6BQZ-TZUFFJO-HO6KAJV-YT2KMAJ"; };
         "jdy-samsung" = { id = "Y7224Y7-NQW7NB5-H74YYVA-KG5DHI4-MVAWMMG-INMA4J3-HYVH5RW-4SBPUQ2"; };
-        # "jdy-laptop" = { id = ""; };
+        "jdy-laptop" = { id = "SIAZJVL-VTIHLLW-6INP5DI-LNFDZIR-FX3XXJM-UWH45UV-A4IQYO3-BPMLIQ3"; };
       };
 
       folders = {
@@ -27,7 +27,7 @@
             "jdy-desktop" 
             "jdy-samsung" 
             "oracle-main" 
-            # "jdy-laptop"
+            "jdy-laptop"
           ];
         };
         "congealed-magma" = {
@@ -37,7 +37,7 @@
             "jdy-desktop" 
             "jdy-samsung" 
             "oracle-main" 
-            # "jdy-laptop"
+            "jdy-laptop"
           ];
         };
       };
