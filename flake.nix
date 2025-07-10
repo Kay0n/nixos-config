1345bfd75ec8c16dd2068f740957325a351a1649
Kay0n <kayon5555@gmail.com>
2025-07-10 21:08:23 +0930

    uncomment split

--- flake.nix
+++ flake.nix
@@ -2,18 +2,19 @@
   description = "Kayon's NixOS Configurations - Root Flake";
 
   inputs = {
-    # jdy-laptop.url = "path:./hosts/jdy-laptop";
-    # mv-church.url = "path:./hosts/mv-church";
+    jdy-laptop.url = "path:./hosts/jdy-laptop";
+    mv-church.url = "path:./hosts/mv-church";
     oracle-main.url = "path:./hosts/oracle-main";
-    # jdy-desktop.url = "path:./hosts/jdy-desktop";
+    jdy-desktop.url = "path:./hosts/jdy-desktop";
   };
 
   outputs = { self, ... }@inputs: {
     nixosConfigurations = {
-      # jdy-laptop = inputs.jdy-laptop.nixosConfigurations.jdy-laptop;
-      # mv-church = inputs.mv-church.nixosConfigurations.mv-church;
+      jdy-laptop = inputs.jdy-laptop.nixosConfigurations.jdy-laptop;
+      mv-church = inputs.mv-church.nixosConfigurations.mv-church;
       oracle-main = inputs.oracle-main.nixosConfigurations.oracle-main;
-      # jdy-desktop = inputs.jdy-desktop.nixosConfigurations.jdy-desktop;
+      jdy-desktop = inputs.jdy-desktop.nixosConfigurations.jdy-desktop;
     };
   };
 }
+
